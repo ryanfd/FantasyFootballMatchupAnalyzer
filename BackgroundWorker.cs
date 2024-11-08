86a19e7583f0eafb957f42a5861a1b8507ab9bfa
ryanfd <rdonnelly98@gmail.com>
2024-11-08 13:18:26 -0800

    Add project files.

---
+++ BackgroundWorker.cs
@@ -0,0 +1,23 @@
+﻿using Blazor.BrowserExtension;
+using System.Threading.Tasks;
+
+namespace FantasyFootballMatchupAnalyzer
+{
+    public partial class BackgroundWorker : BackgroundWorkerBase
+    {
+        [BackgroundWorkerMain]
+        public override void Main()
+        {
+            WebExtensions.Runtime.OnInstalled.AddListener(OnInstalled);
+        }
+
+        async Task OnInstalled()
+        {
+            var indexPageUrl = WebExtensions.Runtime.GetURL("index.html");
+            await WebExtensions.Tabs.Create(new()
+            {
+                Url = indexPageUrl
+            });
+        }
+    }
+}
