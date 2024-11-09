1ccde91ee59885fa0b1807329d4bca1fc51ef62d
ryanfd <rdonnelly98@gmail.com>
2024-11-09 11:48:12 -0800

    Getting some data, but not all

--- Classes/UserInfoModel.cs
+++ Classes/UserInfoModel.cs
@@ -6,52 +6,80 @@ namespace UserInfo
 {
     public partial class UserInfoModel
     {
-        private string displayName;
-
-        [JsonProperty("user_id")]
-        public string UserId { get; }
+        public string AvatarDisplayLink => $"https://sleepercdn.com/avatars/thumbs/{Avatar}";
 
         [JsonProperty("avatar")]
-        public string Avatar { get; }
+        public string Avatar { get; set; }
 
-        public UserInfoModel(string displayName)
-        {
-            this.displayName = displayName;
-        }
+        [JsonProperty("cookies")]
+        public object Cookies { get; set; }
 
-        //{
-        //"avatar":"e7af4deab0289b4f5505646424895246",
-        //"cookies":null,
-        //"created":null,
-        //"currencies":null,
-        //"data_updated":null,
-        //"deleted":null,
-        //"display_name":"RDonnelly",
-        //"email":null,
-        //"is_bot":false,
-        //"metadata":null,
-        //"notifications":null,
-        //"pending":null,
-        //"phone":null,
-        //"real_name":null,
-        //"solicitable":null,
-        //"summoner_name":null,
-        //"summoner_region":null,
-        //"token":null,
-        //"user_id":"986817661701054464",
-        //"username":"rdonnelly",
-        //"verification":null
-        //   }
+        [JsonProperty("created")]
+        public object Created { get; set; }
+
+        [JsonProperty("currencies")]
+        public object Currencies { get; set; }
+
+        [JsonProperty("data_updated")]
+        public object DataUpdated { get; set; }
+
+        [JsonProperty("deleted")]
+        public object Deleted { get; set; }
+
+        [JsonProperty("display_name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty("email")]
+        public object Email { get; set; }
+
+        [JsonProperty("is_bot")]
+        public bool IsBot { get; set; }
+
+        [JsonProperty("metadata")]
+        public object Metadata { get; set; }
+
+        [JsonProperty("notifications")]
+        public object Notifications { get; set; }
+
+        [JsonProperty("pending")]
+        public object Pending { get; set; }
+
+        [JsonProperty("phone")]
+        public object Phone { get; set; }
+
+        [JsonProperty("real_name")]
+        public object RealName { get; set; }
+
+        [JsonProperty("solicitable")]
+        public object Solicitable { get; set; }
+
+        [JsonProperty("summoner_name")]
+        public object SummonerName { get; set; }
+
+        [JsonProperty("summoner_region")]
+        public object SummonerRegion { get; set; }
+
+        [JsonProperty("token")]
+        public object Token { get; set; }
+
+        [JsonProperty("user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("verification")]
+        public object Verification { get; set; }
     }
 
     public partial class UserInfoModel
     {
-        public static UserInfoModel FromJson(string json) => JsonConvert.DeserializeObject<UserInfoModel>(json, UserInfo.Converter.Settings);
+        public static UserInfoModel FromJson(string json) => JsonConvert.DeserializeObject<UserInfoModel>(json, Converter.Settings);
     }
 
     public static class Serialize
     {
-        public static string ToJson(this UserInfoModel self) => JsonConvert.SerializeObject(self, UserInfo.Converter.Settings);
+        public static string ToJson(this UserInfoModel self) => JsonConvert.SerializeObject(self, Converter.Settings);
     }
 
     internal static class Converter
